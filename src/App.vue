26adc17275d2db3bac82810b17c8e3a6f530dd43
Fatoumacisse99 <cfama673@gmail.com>
2024-08-09 12:13:24 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <NavbarComponent msg="Welcome to Your Vue.js App"/>
+    <JumbotronComponent/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavbarComponent from './components/NavbarComponent.vue'
+import JumbotronComponent from './components/JumbotronComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavbarComponent,
+    JumbotronComponent
   }
 }
 </script>
@@ -21,6 +25,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
---
+++ src/components/JumbotronComponent.vue
@@ -0,0 +1,25 @@
+<template>
+ <div class="container">
+  <div class="row">
+  <div class="col-md-7"><img src="https://picsum.photos/id/237/600/400" alt=""></div>
+  <div class="col-md-5">
+    <h1 class="font-weigh-light">Tagline</h1>
+    <p class="mt-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut odio eum aliquid, illum doloremque voluptatem rerum accusantium commodi eveniet! Ipsam illo qui quis voluptatibus ducimus architecto veritatis, quas officia maxime!</p>
+  </div>
+
+ </div>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'JumbotronComponent',
+  
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
